248e4f2e74c6044ca26c8eaae5046ed517059cb8
ADMIN <vinoj455@gmail.com>
2022-10-09 08:40:29 +0530

    category

---
+++ src/main/java/com/project/entity/Category.java
@@ -0,0 +1,24 @@
+package com.project.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="categories")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int categoryId;
+	private String categoryName;
+}
